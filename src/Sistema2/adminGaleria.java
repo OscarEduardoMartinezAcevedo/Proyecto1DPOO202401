0a21507dfce8e1044fdfc78e073ed2a417db2203
chabelycalvo <111368075+chabelycalvo@users.noreply.github.com>
2024-05-11 19:29:57 -0500

    Admin - creacion de pieza llamando metodo

--- src/Sistema2/adminGaleria.java
+++ src/Sistema2/adminGaleria.java
@@ -1,5 +1,7 @@
 package Sistema2;
 
+import Sistema1.CreacionPiezas; //aca el creacion de obras es el metodo que crea una obra con la informacion que se necesite de esta 
+
 public class adminGaleria extends Usuario{
 
 	public adminGaleria(int idUsuario, String nombre, int telefono, String correo, String login, String password) {
@@ -7,4 +9,23 @@ public class adminGaleria extends Usuario{
 		
 	}
 
-}
+public void crearPieza(String Titulo, int Anno, String lugarCreacion, String idPieza, boolean disponibleValorFijo, boolean piezaBloqueada, boolean piezaVendida, double valorMinimo, double valorInicial, String ubicacion, double alto, double ancho, String tecnica, String autor, double profundidad, String materiales, double peso, String detallesInstalacion, String detallesPresentacion, String duracion  ){
+	if (tipo.equalsIgnoreCase("Pintura")){
+
+		CreacionPiezas.crearPieza(Titulo, Anno, lugarCreacion, idPieza, disponibleValorFijo, piezaBloqueada, piezaVendida, valorMinimo, valorInicial, ubicacion, alto, ancho, tecnica, autor);
+
+	}else if (tipo.equalsIgnoreCase("Esculturas")){
+		CreacionPiezas.crearPieza(Titulo, Anno, lugarCreacion, idPieza, disponibleValorFijo, piezaBloqueada, piezaVendida, valorMinimo, valorInicial, ubicacion, alto, ancho, profundidad, materiales, peso, detallesInstalacion);
+	}else if (tipo.equalsIgnoreCase("Videos")){
+		CreacionPiezas.crearPieza(Titulo, Anno, lugarCreacion, idPieza, disponibleValorFijo, piezaBloqueada, piezaVendida, valorMinimo, valorInicial, ubicacion, duracion, autor, materiales, detallesPresentacion);
+	}else if (tipo.equals.crearPieza("Fotografias")){
+		CreacionPiezas.crearPieza(Titulo, Anno, lugarCreacion, idPieza, disponibleValorFijo, piezaBloqueada, piezaVendida, valorMinimo, valorInicial, ubicacion, duracion, autor, materiales, detallesPresentacion);
+	}else if (tipo.equalsIgnoreCase("Impresiones")){
+		CreacionPiezas.crearPieza(Titulo, Anno, lugarCreacion, idPieza, disponibleValorFijo, piezaBloqueada, piezaVendida, valorMinimo, valorInicial, ubicacion, alto, ancho, materiales, detallesInstalacion );
+	} else {
+	System.out.println("Tipo de pieza no reconocido");
+	}
+		}
+
+	}
+
